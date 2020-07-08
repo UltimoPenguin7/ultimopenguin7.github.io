a8120931656bc36baf396ddf86f9825a082487ba
UltimoPenguin7 <ultimopenguin7@gmail.com>
2020-07-07 19:29:33 -0600

    Update Copy LGHUB script for new LGHUB LocalAppData path

--- scripts/copy-logitech-g-hub.ps1
+++ scripts/copy-logitech-g-hub.ps1
@@ -1,9 +1,11 @@
-$source = Join-Path $env:LOCALAPPDATA "\Logitech\Logitech Gaming Software"
+# $source = Join-Path $env:LOCALAPPDATA "\Logitech\Logitech Gaming Software"
+# Updated LGHUB Path on 2020-07-07
+$source = Join-Path $env:LOCALAPPDATA "\LGHUB\"
 $parentPath = (get-item $PSScriptRoot).parent;
 # $parentPath = ls "..\.";
 # $dest = Join-Path $parentPath (Res"snippets\logitech-g-hub"
-$dest = Resolve-Path "..\snippets\logitech-g-hub"
-Copy-Item $source $dest -force
+$dest = Resolve-Path "..\snippets\LGHUB"
+Copy-Item $source\* $dest -force
 Write-Output "Copy from ``$source`` to ``$dest`` Done."
-cd ..
-git status
+# cd ..
+# (cd ..\; git status);
